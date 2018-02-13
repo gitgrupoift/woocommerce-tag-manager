8f20677901fe4be884b2b9da538b1bd386b4eabf
Víctor Alonso <works.alonsog@gmail.com>
2018-02-13 13:01:15 +0100

    Error en la impresión de ID´s en el checkout

--- views/checkout.php
+++ views/checkout.php
@@ -2,7 +2,7 @@
 // A function to handle a click on a checkout button. This function uses the eventCallback
 // data layer variable to handle navigation after the ecommerce data has been sent to Google Analytics.
 dataLayer.push({
-  'ecomm_prodid': '<?= $products_remarketing ?>',
+  'ecomm_prodid': '[<?= implode(',',$products_remarketing) ?>]',
   'ecomm_pageType': 'cart',
   'ecomm_totalValue': '<?= $totalvalue ?>'
   'event': 'checkout',
